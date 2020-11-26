38652dae41bf297374aed3bdbff547e7a3fcc65b
Emily Willow <emily.shapiera@gmail.com>
2020-11-26 14:18:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
-  def person_name=(person_name)
+  def name=(person_name)
     @this_persons_name = person_name
   end
   def person_name
     @this_persons_name
   end
-  def persons_job=(person_job)
+  def job=(person_job)
     @this_persons_job = person_job
   end
   def person_job
