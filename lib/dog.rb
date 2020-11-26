172eac7cd2bf5f43defabff8ca7d8bc094ca46cd
Emily Willow <emily.shapiera@gmail.com>
2020-11-26 14:21:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-end
-def name
-  @this_dogs_name
+def initialize(dogs_name)
+  @this_dogs_name = dogs_name
+
+  def name
+    @this_dogs_name
+  end
+
+  def name=(dogs_new_name)
+    @this_dogs_name = dogs_new_name
 end
 
 def breed=(breed_name)
